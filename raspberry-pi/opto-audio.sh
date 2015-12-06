f17676a3ca5cb864088a96bcbf3ad91c3ac6fc32
markten <minty101011@gmail.com>
2015-12-06 09:41:12 -0500

    renamed init file

--- raspberry-pi/opto-audio.sh
+++ raspberry-pi/opto-audio.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ### BEGIN INIT INFO
-# Provides:          opto-audio
-# Required-Start:    $all
-# Required-Stop:
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
+# Provides: 		opto-audio 
+# Required-Start: 	$all 
+# Required-Stop:	 
+# Default-Start: 	2 3 4 5 
+# Default-Stop: 	0 1 6 
 # Short-Description: Starts opto-audio service
 ### END INIT INFO
 
@@ -32,11 +32,9 @@ case "$1" in
        start
        ;;
     status)
-       # code to check status of app comes here
-       # example: status program_name
+       # code to check status of app comes here example: status program_name
        ;;
     *)
-       echo "Usage: $0 {start|stop|status|restart}"
-esac
-
+	echo "Usage: $0 {start|stop|status|restart}" 
+	esac
 exit 0
